aaaf120d3e576bcd8025328fbd67422c61850cbe
Oluwaseyi Akinsanya <103059410+zooviee@users.noreply.github.com>
2024-01-03 17:26:40 +0100

    Add files via upload

---
+++ Day_1/band_name_generator_project.py
@@ -0,0 +1,12 @@
+# Create a greeting for your program.
+print("Welcome to the Band Name Generator")
+# Ask the user for the city that they grew up in.
+city_name = input("What's the name of the city you grew up in?\n")
+# Ask the user for the name of a pet.
+pet_name = input("What's your pet's name?\n")
+# Combine the name of their city and pet and show them their band name.
+band_name = city_name + " " + pet_name
+# Print band name:
+print(f"Your band name could be: {band_name}.")
+
+# This is the end of program
